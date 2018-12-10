5e472ec8bbf7637aaa26aae8eb20b9ede70eb33f
Tariq Patel <tariq.patel@estalea.com>
2018-12-10 20:13:28 +0200

    Initial Commit of project

---
+++ MyCoinJarApp/MyCoinJarApp.Core/Models/RadioButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyCoinJarApp.Core.Models
+{
+    public class RadioButton
+    {
+        public string Caption { get; set; }
+        public RadioButton(string caption)
+        {
+            Caption = caption;
+        }
+    }
+}
---
+++ MyCoinJarApp/MyCoinJarApp.Core/ViewModels/FirstViewModel.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using MvvmCross.Core.ViewModels;
+using MyCoinJarApp.Core.Models;
+
+namespace MyCoinJarApp.Core.ViewModels
+{
+    public class FirstViewModel
+        : MvxViewModel
+    {
+        public MvxCommand AddCoinToJarCommand;
+
+        public FirstViewModel()
+        {
+            AddCoinToJarCommand = new MvxCommand(DoAddCoinToJar);
+        }
+
+        void DoAddCoinToJar()
+        {
+
+        }
+
+        public string AddToJarString => "Add Coin";
+
+        private List<RadioButton> _items = new List<RadioButton>()
+            {
+                new RadioButton("One"),
+                new RadioButton("Two"),
+                new RadioButton("Three"),
+                new RadioButton("Four"),
+            };
+        public List<RadioButton> Items
+        {
+            get { return _items; }
+            set { _items = value; RaisePropertyChanged(() => Items); }
+        }
+
+        private RadioButton _selectedItem = new RadioButton("One");
+        public RadioButton SelectedItem
+        {
+            get { return _selectedItem; }
+            set { _selectedItem = value; RaisePropertyChanged(() => SelectedItem); }
+        }
+    }
+}
